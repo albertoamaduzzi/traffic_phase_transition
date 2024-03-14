931121e56c9a1cae3a619c0d35d7181e22c322e9
Alberto Amaduzzi <alberto@artemis.ist.berkeley.edu>
2024-03-14 00:10:00 +0000

    Ready to change all the mass

--- scripts/PreProcessing/Potential.py
+++ scripts/PreProcessing/Potential.py
@@ -10,6 +10,7 @@ import numba
 import logging
 import pandas as pd
 import ast
+import math
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 sys.path.append('~/berkeley/traffic_phase_transition/scripts')
@@ -449,14 +450,6 @@ def GetCommunicationLevelsAmongGrids(Tij):
 #####                                   SaveVectorField(VectorField,VectorFieldDir)
 
 def parse_dir_vector(vector_string):
-    '''
-        Input:
-            String of the index in df_distance
-        Output:
-            (i,j): tuple (int,int)
-        Usage:
-            Guarantees the multiplication df_distance*Tij for fluxes vector
-    '''
     if vector_string== '[nan,nan]' or vector_string== '[nan nan]':
         vector_array = np.array([0,0])
     # Split the string representation of the vector
@@ -467,11 +460,8 @@ def parse_dir_vector(vector_string):
     return vector_array
 
 def GetVectorField(Tij,df_distance):
-    '''
-        Input:
-            Tij: ['']
-    '''
     Tij['vector_flux'] = df_distance['dir_vector'].apply(lambda x: parse_dir_vector(x) ) * Tij['number_people']
+
     # Create VectorField DataFrame
     VectorField = pd.DataFrame(index=Tij['(i,j)D'].unique(), columns=['(i,j)', 'Ti', 'Tj'])
     Tj_values = Tij.groupby('(i,j)D')['vector_flux'].sum()
@@ -480,13 +470,14 @@ def GetVectorField(Tij,df_distance):
     # Calculate 'Ti' values
     Ti_values = Tij.groupby('(i,j)O')['vector_flux'].sum()
     VectorField['Ti'] = Ti_values
+    VectorField['index'] = VectorField.index
     VectorField['(i,j)'] = VectorField['index']
     VectorField['index'] = VectorField.index
     VectorField.reset_index(inplace=True)
     return VectorField
 
 def SaveVectorField(VectorField,save_dir):
-    VectorField.to_csv(os.path.join(save_dir,'VectorField.csv'))    
+    VectorField.to_csv(os.path.join(save_dir,'VectorField.csv'))
 
 def GetSavedVectorFieldDF(save_dir):
     return pd.read_csv(os.path.join(save_dir,'VectorField.csv'))
@@ -506,12 +497,22 @@ def GetPotentialLattice(lattice,VectorField):
             lattice with:
                 'V_in' potential for the incoming fluxes
                 'V_out' potential for the outgoing fluxes
+                'rotor_z_in': Is the rotor at the point (i,j) for the ingoing flux. (Tj) sum over i. So I look at a source and I say that the field
+                              is the ingoing flux. This is strange as it does not give any information about where to go to find the sink.
+                'rotor_z_out': Is the rotor at the point (i,j) for the ingoing flux. (Ti) sum over j. So I look at a source and I say that the field
+                               is the outgoing flux. In this way I am considering the analogue case to the google algorithm for
+                               page rank as I am at a random point and the field points at the direction with smaller potential, the sink, that is 
+                               the higher rank of importance.
+                    
         Describe:
             Output = Input for ConvertLattice2PotentialDataframe
     '''
     nx.set_node_attributes(lattice, 0, 'V_in')
     nx.set_node_attributes(lattice, 0, 'V_out')
     nx.set_node_attributes(lattice, 0, 'index')
+    nx.set_node_attributes(lattice, 0, 'rotor_z_out')
+    nx.set_node_attributes(lattice, 0, 'rotor_z_in')
+
     max_i = max(ast.literal_eval(node_str)[0] for node_str in lattice.nodes)
     max_j = max(ast.literal_eval(node_str)[1] for node_str in lattice.nodes)
     for node_str in lattice.nodes:    
@@ -526,11 +527,52 @@ def GetPotentialLattice(lattice,VectorField):
         node_index_1, node_index_2 = edge[:2]    
         VectorField.index = VectorField['(i,j)']
         # Compute the value of V for the edge using the formula
-        node_Vin = lattice.nodes[node_index_1]['V_in'] + lattice[node_index_1][node_index_2]['dx'] * VectorField.loc[node_index_1, 'Tj'][0]  + lattice[node_index_1][node_index_2]['dy'] * VectorField.loc[node_index_1, 'Tj'][1]  
-        node_Vout = lattice.nodes[node_index_1]['V_out'] + lattice[node_index_1][node_index_2]['dx'] * VectorField.loc[node_index_1, 'Ti'][0]  + lattice[node_index_1][node_index_2]['dy'] * VectorField.loc[node_index_1, 'Ti'][1]      
+        # NOTE: The formula seems to have dx multiplying the y component of the vector field (it is not the case as (0,0),(1,0) is moving in x)
+        # NOTE: I computed dx thinking that (1,0) being the y !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  dx is dy
+        # NOTE: CHECK df_distance
+        dxTjx = lattice[node_index_1][node_index_2]['dy'] * VectorField.loc[node_index_1, 'Tj'][0]
+        dyTjy = lattice[node_index_1][node_index_2]['dx'] * VectorField.loc[node_index_1, 'Tj'][1]
+        node_Vin = lattice.nodes[node_index_1]['V_in'] + dxTjx  + dyTjy  
+        node_Vout = lattice.nodes[node_index_1]['V_out'] + lattice[node_index_1][node_index_2]['dx'] * VectorField.loc[node_index_1, 'Ti'][1]  + lattice[node_index_1][node_index_2]['dy'] * VectorField.loc[node_index_1, 'Ti'][0]      
+        # ROTOR NOTE: AGAIN[d/dx is d/dy] NETWORKX USES THIS CONVENTION
+        if not math.isinf(lattice[node_index_1][node_index_2]['d/dy']):
+            ddxTjy = lattice[node_index_1][node_index_2]['d/dy'] * VectorField.loc[node_index_1, 'Tj'][1]
+        else:
+            ddxTjy = 0
+        if not math.isinf(lattice[node_index_1][node_index_2]['d/dx']):
+            ddyTjx = lattice[node_index_1][node_index_2]['d/dx'] * VectorField.loc[node_index_1, 'Tj'][0]  
+        else:
+            ddyTjx = 0
+        rotor_z_in = ddxTjy  - ddyTjx
+        if not math.isinf(lattice[node_index_1][node_index_2]['d/dy']):    
+            ddxTiy = lattice[node_index_1][node_index_2]['d/dy'] * VectorField.loc[node_index_1, 'Ti'][1]
+        else:
+            ddxTiy = 0
+        if not math.isinf(lattice[node_index_1][node_index_2]['d/dx']):
+            ddyTix = lattice[node_index_1][node_index_2]['d/dx'] * VectorField.loc[node_index_1, 'Ti'][0]  
+        else:
+            ddyTix = 0
+        rotor_z_out = ddxTiy  - ddyTix
         lattice.nodes[node_index_2]['V_in'] = node_Vin
         lattice.nodes[node_index_2]['V_out'] = node_Vout
         lattice.nodes[node_index_2]['index'] = VectorField.loc[node_index_1, 'index']
+        lattice.nodes[node_index_2]['rotor_z_in'] = rotor_z_in
+        lattice.nodes[node_index_2]['rotor_z_out'] = rotor_z_out
+    return lattice
+
+def SmoothPotential(lattice):
+    # Smooth V_in and V_out by taking the average over all the neighbors
+    for node_str in lattice.nodes:
+        neighbors = list(lattice.neighbors(node_str))
+        num_neighbors = len(neighbors)
+        
+        # Calculate the average of V_in and V_out for the neighbors
+        avg_Vin = sum(lattice.nodes[neighbor]['V_in'] for neighbor in neighbors) / num_neighbors
+        avg_Vout = sum(lattice.nodes[neighbor]['V_out'] for neighbor in neighbors) / num_neighbors
+        
+        # Assign the average values to the current node
+        lattice.nodes[node_str]['V_in'] = avg_Vin
+        lattice.nodes[node_str]['V_out'] = avg_Vout    
     return lattice
 
 def ConvertLattice2PotentialDataframe(lattice):
@@ -557,14 +599,21 @@ def ConvertLattice2PotentialDataframe(lattice):
         x = lattice.nodes[node]['x']
         y = lattice.nodes[node]['y']
         index_ = lattice.nodes[node]['index']
+        rotor_z_in = lattice.nodes[node]['rotor_z_in']
+        rotor_z_out = lattice.nodes[node]['rotor_z_out']
         # Save the information to the list
-        data_.append({'V_in': node_Vin, 'V_out': node_Vout,'index': index_ ,'node_id': node_id,'x':x,'y':y})
+        data_.append({'V_in': node_Vin, 'V_out': node_Vout,'index': index_ ,'node_id': node_id,'x':x,'y':y,'rotor_z_in':rotor_z_in,'rotor_z_out':rotor_z_out})
         
         # Create a DataFrame from the list
         PotentialDataframe = pd.DataFrame(data_)
-
+        PotentialDataframe['index'] = PotentialDataframe.index
         # Format the 'node_id' column using ast.literal_eval
 #        PotentialDataframe['node_id'] = PotentialDataframe['node_id'].apply(ast.literal_eval)
+    return PotentialDataframe
+
+def CompletePotentialDataFrame(VectorField,grid,PotentialDataframe):
+    PotentialDataframe['Ti'] = VectorField['Ti']
+    PotentialDataframe['population'] = grid['population']    
     return PotentialDataframe
 
 def SavePotentialDataframe(PotentialDataFrame,save_dir):
@@ -572,71 +621,6 @@ def SavePotentialDataframe(PotentialDataFrame,save_dir):
 
 def GetSavedPotentialDF(save_dir):
     return pd.read_csv(os.path.join(save_dir,'PotentialDataFrame.csv'))
-
-def OD2matrix(DfGrid,direction_matrix,gridIdx2ij,lattice,grid):
-    '''
-        Create: 
-            1) 02mapidxorigin {0:origin1,1:origin2,...}
-            2) 02mapidxdestination {0:destination1,1:destination2,...}
-            3) mapidx2origin {origin1:0,origin2:1,...}
-            4) mapidx2destination {destination1:0,destination2:1,...}
-            origin_i,destination_i are the nominative of self.gdf_polygons
-    '''
-    origins = np.unique(DfGrid['origin'].to_numpy())
-    destinations = np.unique(DfGrid['destination'].to_numpy())
-    mapOD = DfGrid.groupby('origin')['destination'].unique().apply(lambda x: x[0]).to_dict()
-    zero2mapidxorigin = {i:idx for idx,i in enumerate(origins)}
-    zero2mapidxdestination = {i:idx for idx,i in enumerate(destinations)}
-    mapidx2origin = {idx:i for idx,i in enumerate(origins)}
-    mapidx2destination = {idx:i for idx,i in enumerate(destinations)}
-    dimensionality_matrix = (len(origins),len(destinations))
-    matrix_OD = np.zeros(dimensionality_matrix)
-    trips_OD = DfGrid.groupby(['origin']).count()['destination'].to_numpy(dtype= int)
-    for idx in range(len(origins)):
-        matrix_OD[zero2mapidxorigin[origins[idx]],zero2mapidxdestination[mapOD[origins[idx]]]] = trips_OD[idx]
-    matrix_out = np.sum(matrix_OD,axis=1)
-    matrix_in = np.sum(matrix_OD,axis=0)
-    return matrix_OD,matrix_out,matrix_in,zero2mapidxorigin,zero2mapidxdestination,mapidx2origin,mapidx2destination
-
-
-
-def GradientFromPotential(grid,
-                          lattice):
-    '''
-        Input:
-            dfOD: (pd.DataFrame) [i,j,Flux,'(i,j)O','(i,j)D']
-            LatticeGrid: (nx.Graph) LatticeGrid NODES: [(i,j),'centroidx','centroidy'] EDGES: ['d\dx','d\dy','dx','dy']
-        Create the vector field from the potential
-    '''
-    point_coords = np.array([np.array([lattice.nodes[node]['x'],lattice.nodes[node]['y']]) for node in lattice.nodes() if 'x' in lattice.nodes[node].keys() and 'y' in lattice.nodes[node].keys()]) #SHAPE (ncenters,2)
-    points_3d = point_coords[:, np.newaxis, :]
-    Directions = point_coords - points_3d #SHAPE (ncenters,ncenters,2)`
-    Dij = distance_matrix(point_coords,point_coords) #SHAPE (ncenters,ncenters)
-#    Dij =  np.linalg.norm(Directions, axis=2)[:, :, np.newaxis] #SHAPE (ncenters,ncenters,1)
-#    Dij = np.array([np.array([Dij[i][j][0] if Dij[i][j][0] != 0 else 1 for j in range(len(Dij[i]))]) for i in range(len(Dij))]) #SHAPE (ncenters,ncenters)
-
-    eij = [[Directions[i][j] / Dij[i][j] for j in range(len(Directions[i]))] for i in range(len(Directions))] #SHAPE (ncenters,ncenters,2)
-    not_yet_flux_vectors = np.array([np.array([eij[i][j]*fluxes_matrix[i][j] for j in range(len(eij[0]))]) for i in range(len(eij))])
-    flux_vectors = np.sum(not_yet_flux_vectors,axis=1) #SHAPE (ncenters,2)
-    mass = np.sum(matrix_out)
-
-
-def Lorenz(DensityVector):
-    '''
-        Lorenz curve is the comulative probability distribution function of the density (In our case potential).
-        Output:
-            CDF: vector(float) -> Increases
-            SortedByDensity2PrimitiveIndex: dict -> {0: idx DensityVector that is smallest,....,n: Idx DensityVector that is biggest}
-    '''
-    Dv = np.argsort(DensityVector)
-    SortedByDensity2PrimitiveIndex = {idx: Dv[idx] for idx in range(len(Dv))}
-    Z = np.sum(Dv)
-    CDF = np.cumsum(DensityVector)/Z
-    return CDF,SortedByDensity2PrimitiveIndex
-
-    
-
-
 
 
 
@@ -661,3 +645,56 @@ def DebuggingGetd0Celli(count_i,NCycleControl,VnpeopleXi,VoriginsXi,Vdestination
             logging.info(f'Indices Origin:\n{VoriginsXi}')
             logging.info(f'Indices Destination:\n{VdestinationsXi}')
             count_i += 1
+
+
+
+##------------------------------------------------ RANDOM FUNCTIONS ----------------------------------------------#
+def filter_within_percentage(arr, lower_percentile, upper_percentile):
+    # Calculate the lower and upper percentile values
+    lower_val = np.percentile(arr, lower_percentile)
+    upper_val = np.percentile(arr, upper_percentile)
+    
+    # Filter values within the specified percentile range
+    filtered_values = arr[(arr >= lower_val) & (arr <= upper_val)]
+    
+    return filtered_values
+
+
+
+##-------------------------------------------- PLOTS ----------------------------------------------------------#
+
+def PlotVFPotMass(grid,SFO_obj,PotentialDataframe,VectorField,label_potential = 'V_in',label_fluxes = 'Tj'):
+    '''
+        NOTE:
+            label_potential:    V_in, V_out
+            label_fluxes:       Tj  , Ti
+        USAGE:
+            PlotVFPotMass(grid,SFO_obj,PotentialDataframe,VectorField,label_potential = 'V_out',label_fluxes = 'Ti')
+            PlotVFPotMass(grid,SFO_obj,PotentialDataframe,VectorField,label_potential = 'population',label_fluxes = 'Ti')
+
+    '''
+    labelf2title = {'Tj': 'Incoming','Ti':'Outgoing'}
+    label2save = {'V_in':'Potential','V_out':'Potential','population':'Mass'}
+    fig, ax = plt.subplots(figsize=(15, 15))
+    centroid_coords = np.array([grid['centroidx'].to_numpy(),grid['centroidy'].to_numpy()])
+    centroid_coords = centroid_coords.T
+    #grav_vector_field = gravitational_field(fluxes_matrix,normalized_vectors,nv)
+    SFO_obj.gdf_polygons.plot(ax=ax, color='white', edgecolor='black')
+    if label_potential in PotentialDataframe.columns: 
+        grid[label_potential] = PotentialDataframe[label_potential]
+    elif label_potential in grid.columns:
+        pass
+    else:
+        raise KeyError(label_potential,' Neither in grid nor potential columns: ',grid.columns,PotentialDataframe.columns)
+    grid_plot = grid.plot(ax=ax, column = label_potential, cmap = 'viridis',edgecolor='black', alpha=0.3)
+    grid_cbar = plt.colorbar(grid_plot.get_children()[1], ax=ax)
+    grid_cbar.set_label('{}'.format(label_potential), rotation=270, labelpad=15)
+    VF = np.stack(VectorField[label_fluxes].to_numpy(dtype = np.ndarray))
+    VF_norm = np.linalg.norm(VF, axis=1)
+    VF_normalized = np.stack(np.array([VF[i] / VF_norm[i] if VF_norm[i] !=0 else [0, 0] for i in range(len(VF_norm))]))
+    quiver_plot = ax.quiver(centroid_coords[:,0], centroid_coords[:,1], VF_normalized[:,0], VF_normalized[:,1],
+            VF_norm, cmap='inferno', angles='xy', scale_units='xy', scale=50, width=0.005,headwidth=1, headlength=2)
+    quiver_cbar = plt.colorbar(quiver_plot, ax=ax)
+    quiver_cbar.set_label('Normalized Vector Magnitude', rotation=270, labelpad=15)
+    ax.set_title('Vector Field {} Fluxes'.format(labelf2title[label_fluxes]))
+    plt.savefig(os.path.join(dir_grid,'{0}Flux{1}.png'.format(labelf2title[label_fluxes],label2save[label_potential])),dpi = 200)
